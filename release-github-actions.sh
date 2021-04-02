d316867e8ee3008421d87fa8b9f8a605a9a2b047
Mariusz Wyszomierski <mariusz.wyszomierski@coi.gov.pl>
2021-04-02 22:39:55 +0200

    Update release-github-actions.sh

--- release-github-actions.sh
+++ release-github-actions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 export CI_COMMIT_SHA="${GITHUB_SHA}"
+echo "CI_COMMIT_SHA=${GITHUB_SHA}"
 export CI_COMMIT_REF_NAME="${GITHUB_REF}"
+echo "CI_COMMIT_REF_NAME=${GITHUB_REF}"
+echo "----"
+printenv 
 
 release.sh
